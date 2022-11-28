08e4e33beaeee29a3f070db925a7b5183f3a146e
Jake Peterson <a3sir86@gmail.com>
2022-11-28 12:49:06 -0600

    first commit

---
+++ comments_preproc.c
@@ -0,0 +1,16 @@
+// Preprocessor, denoted with # - part of c compilation that recognizes special statements, analyzes these statements before analysis of the C progress itself, instruction to compiler to do something before compiling 
+// can be used to create constants and macros with #define
+// include - library (reusing existing code) files
+// conditionals - ifdef, endif, else, ifndef
+#include <stdio.h>
+
+//single line comment
+/* 
+multiline 
+comment 
+*/
+
+int main() {
+  printf("Hi my name is Jake");
+  return 0;
+}
