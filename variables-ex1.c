7fa65f5e7a9269e39ac7371fdddad07d6238a3a8
Jake Peterson <a3sir86@gmail.com>
2022-11-30 15:45:42 -0600

    Create variables-ex1.c

---
+++ variables-ex1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  
+  //declare variables
+  double width, height, perimeter, area;
+  
+  //assign variables
+  width = atoi(argv[1]); //atoi converts the string to int
+  height = atoi(argv[2]);
+  perimeter = 0;
+  area = 0;
+  
+  //calculations
+  perimeter = (width + height) * 2;
+  area = width * height;
+  
+  //output
+  printf("The perimeter of your rectangle with height, %.2f, and width, %.2f is: %lf\n", height, width, perimeter);
+  printf("The area of your rectangle with height, %.2f, and width, %.2f is: %lf\n", height, width, area);
+  
+  return 0;
+}
