bf1a17e18b75c4c6f91ef49b8ef95fd654f144ea
Jake Peterson <a3sir86@gmail.com>
2022-11-30 16:00:23 -0600

    Create operators.c

---
+++ operators.c
@@ -0,0 +1,24 @@
+/**********************************************************
+Notes:
+Operators in C
+  infix stype - inbetween the data
+  Logical Operator/Boolean Operator - Returns boolean based on 2 expressions
+  Arithmetic (+, -, *, /)
+  assignment (=)
+  relational (<, >, !=)
+  bitwise (<<, >>, ~)
+Expressions and Statements in C
+  Statements are the basic program steps of C, most statements are constructed from expressions
+  Expression - consists of a combination of operators and operands
+    operands are what the operator operates on
+    operands can be constant, variables, or combination of two
+    every expression has a value
+**********************************************************/
+
+#include <stdio.h>
+
+int main() {
+  
+  
+  return 0;
+}
