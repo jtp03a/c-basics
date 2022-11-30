ef87767c7a8bc27cf14cd89af4cc63db662e7602
Jake Peterson <a3sir86@gmail.com>
2022-11-30 15:52:44 -0600

    Create enums-ex1.c

---
+++ enums-ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  //define enum
+  enum Company {GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT};
+  
+  //assign values
+  enum Company company1 = XEROX;
+  enum Company company2 = GOOGLE;
+  enum Company company3 = EBAY;
+  
+  //output
+  printf("%i\n%i\n%i\n", company1, company2, company3);
+  
+  return 0;
+}
