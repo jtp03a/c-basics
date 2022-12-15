3e249e2788290dab79f7a800533e7ccba22f9ecc
Jake Peterson <a3sir86@gmail.com>
2022-12-15 15:16:11 -0600

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -25,12 +25,14 @@ int main() {
   printf("\nEnter the 10 grades:\n");
   
   for(int i = 0; i < count; i++) {
-    printf("%2u> ", + 1);
+    printf("%d> ", i + 1);
     scanf("%d", &grades[i]);
     sum += grades[i];
   }
   
+  average = (float)sum/count;
   
+  printf("\nAverage of the ten grades entered is: %.2f\n", average);
   
   return 0;
 }
