d4db16fe1d615d410e2b69361d34a7815059f86f
Jake Peterson <a3sir86@gmail.com>
2022-12-16 10:33:34 -0600

    reorganized

---
+++ 9-functions.c
@@ -0,0 +1,13 @@
+/**************************************************
+ * Notes - Fuctions
+ * Tips
+ *  Keep functions small and scoped to independent tasks
+ *  Think of function as a black box, think about inputs and outputs
+ * Benefits
+ *  Use to reduce code duplication
+ *  Add modularity/reusability to program
+ *  improves readability
+ *  improves program organization
+ *  divide and conquer approach
+ *   
+ * ************************************************/
