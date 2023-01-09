eda4afa9ffb94c067d00f83a748a73c559f7a174
Jake Peterson <a3sir86@gmail.com>
2023-01-09 16:22:17 -0600

    Update pointers-arraysEx1.c

--- examples/pointers-arraysEx1.c
+++ examples/pointers-arraysEx1.c
@@ -9,5 +9,14 @@ int main(void) {
     printf("multiple[%d] = %c *(p+%d) = %c &multple[%d] = %p p+%d = %p\n", i, multiple[i], i, *(p+i), i, &multiple[i], i, p+i);
   }
   
+  long multiple[] = {15L, 25L, 35L, 45L};
+  long * p2 = multiple;
+  
+  for (int i = 0; i < sizeof(multiple)/sizeof(multiple[0]); i++) {
+    printf("address p+%d (&multiple[%d]): %llu    *(p+%d)   value: %d\n", i, i, (unsigned long long)(p2+i), i, *(p2+i));
+    
+    printf("\n    Type long occupie: %d bytes\n", (int)sizeof(long));
+  }
+  
   return 0;
 }
