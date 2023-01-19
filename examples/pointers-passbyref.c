433a70e5ce80861f375e8040777fde6a4943e06f
Jake Peterson <a3sir86@gmail.com>
2023-01-19 08:27:03 -0600

    Update pointers-passbyref.c

--- examples/pointers-passbyref.c
+++ examples/pointers-passbyref.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int calcSqNum(int * num); //declare a function that takes a pointer to an int
+
+int calcSqNum(int * num) {
+  *num = *num * *num; //square the value pointed to by the num pointer
+}
+
+int main() {
+  
+  int numtoSq = 5; 
+  
+  calcSqNum(&numtoSq); //pass the memory address of numtoSq to the function
+  
+  printf("%d", numtoSq); //shows that the value outside a function was modified by the function
+  
+  return 0;
+}
