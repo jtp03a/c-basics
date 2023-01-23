890ffffb15db083cd2ad784cc7b54c4e1d42dde5
Jake Peterson <a3sir86@gmail.com>
2023-01-23 16:17:40 -0600

    Update dynamic-mem-alloc-ex1.c

--- examples/dynamic-mem-alloc-ex1.c
+++ examples/dynamic-mem-alloc-ex1.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main() {
+  
+  int allocSize = 0;
+  char * text = NULL;
+  
+  printf("How long is the string: \n");
+  
+  scanf("%d", &allocSize);
+  
+  text = (char*)malloc(allocSize * sizeof(char));
+  
+  if (text) {
+      printf("Enter some text: \n");
+      scanf(" ");
+      gets(text);
+      
+      printf("Inputted text is %s\n", text);
+  }
+
+  free(text);
+  text = NULL;
+  
+  return 0;
+}
