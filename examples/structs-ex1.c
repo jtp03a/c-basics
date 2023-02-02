fd19fdb6f41e0b42390cf04cd8103e2041e85beb
Jake Peterson <a3sir86@gmail.com>
2023-02-02 10:52:05 -0600

    Update structs-ex1.c

--- examples/structs-ex1.c
+++ examples/structs-ex1.c
@@ -8,14 +8,12 @@ struct Employee {
 
 int main() {
   
-
-  
   struct Employee emp1 = {"Jake Peterson", 10, 100000.02};
   
   printf("%s, %i, %.2f\n", emp1.name, emp1.hireDate, emp1.salary);
   
   printf("What is the employees name:? ");
-  scanf("%s", &emp1.name);
+  scanf("%s", emp1.name); //dont have to provide address bc it is a character array
   printf("What is the number of the day of the month the employee started? ");
   scanf("%i", &emp1.hireDate);
   printf("What is the employees salary? ");
