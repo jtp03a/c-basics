769e16bb95a786db0b2ae1a7a0ff21b103d5bf6a
Jake Peterson <a3sir86@gmail.com>
2023-02-02 11:05:12 -0600

    Update structs-ex2.c

--- examples/structs-ex2.c
+++ examples/structs-ex2.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
 
 struct item {
-  itemName*;
+  char *itemName;
   int quantity;
+  float price;
+  float amount; //stores quandity times price
+};
+
+void readItem(struct * item);
+void printItem(struct * item);
+
+void readItem(struct * item) {
+  char productName[100];
+  int price;
+  
+  printf("Enter the product name: ");
+  scanf("%s", productName);
+  printf("Enter the produce price: ");
+  scanf("%f", &price);
+  printf("Enter the product quantity: ");
+  scanf("%i", &quantity);
+  
+  item->itemName = prodcutName;
+  item->price = price;
+  item->quantity = quantity;
+  
+}
+
+void printItem(struct * item) {
+  printf("%s, %d, %.2f", item->itemName, item->price, item->quantity);
+}
+
+int main() {
+  
+  struct item ball, *pBall;
+  
+  readItem(pBall);
+  printItem(pBall);
+
+  return 0;
+}
