84b48d80f0d6bf45b2ffeb871b4eb83ca8216903
Jake Peterson <a3sir86@gmail.com>
2023-02-02 11:22:06 -0600

    Update structs-ex2.c

--- examples/structs-ex2.c
+++ examples/structs-ex2.c
@@ -1,45 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct item {
   char *itemName;
   int quantity;
   float price;
-  float amount; //stores quandity times price
+  float amount; //stores quantity times price
 };
 
-void readItem(struct * item);
-void printItem(struct * item);
+void readItem(struct item * i);
+void printItem(struct item * i);
 
-void readItem(struct * item) {
+void readItem(struct item * i) {
   char tempStr[100];
   int price;
   int quantity;
   
-  // Initial memory allocation 
-  item->itemName = (char*)malloc(15 * sizeof(char));
 
-  
   printf("Enter the product name: ");
   scanf("%s", tempStr);
-  strcpy(item->itemName, tempStr);
+  strcpy(i->itemName, tempStr);
   
   printf("Enter the produce price: ");
   scanf("%f", &price);
   printf("Enter the product quantity: ");
   scanf("%i", &quantity);
   
-  item->price = price;
-  item->quantity = quantity;
+  i->price = price;
+  i->quantity = quantity;
   
 }
 
-void printItem(struct * item) {
-  printf("%s, %d, %.2f", item->itemName, item->price, item->quantity);
+void printItem(struct item * i) {
+  printf("%s, %.2f, %i", i->itemName, i->price, i->quantity);
 }
 
 int main() {
   
   struct item ball, *pBall;
+  
+  pBall = &ball;
+  
+  pBall->itemName = (char *)malloc(30 * sizeof(char));
+  
+  if(pBall) {
+      exit(-1);
+  }
   
   readItem(pBall);
   printItem(pBall);
