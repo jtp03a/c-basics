0e4c076952fcb70abaeb9ea75ea622f25298e117
Jake Peterson <a3sir86@gmail.com>
2023-02-02 10:48:48 -0600

    Update structs-ex1.c

--- examples/structs-ex1.c
+++ examples/structs-ex1.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
+struct Employee {
+  char name[20];
+  int hireDate;
+  float salary;
+};
+
 int main() {
   
-  struct Employee {
-    char name[20];
-    int hireDate;
-    float salary;
-  };
+
   
   struct Employee emp1 = {"Jake Peterson", 10, 100000.02};
   
   printf("%s, %i, %.2f\n", emp1.name, emp1.hireDate, emp1.salary);
   
-  struct Employee emp2;
-  
   printf("What is the employees name:? ");
-  scanf("%s", &emp2.name);
+  scanf("%s", &emp1.name);
   printf("What is the number of the day of the month the employee started? ");
-  scanf("%i", &emp2.hireDate);
+  scanf("%i", &emp1.hireDate);
   printf("What is the employees salary? ");
-  scanf("%f", &emp2.salary);
+  scanf("%f", &emp1.salary);
   
-  printf("%s, %i, %.2f\n", emp2.name, emp2.hireDate, emp2.salary);
+  printf("%s, %i, %.2f\n", emp1.name, emp1.hireDate, emp1.salary);
   
   return 0;
 }
