ce487435727948b5a9378918ac1ba62df17a3965
Jake Peterson <a3sir86@gmail.com>
2023-02-02 10:40:29 -0600

    Update structs-ex1.c

--- examples/structs-ex1.c
+++ examples/structs-ex1.c
@@ -3,14 +3,14 @@
 int main() {
   
   struct Employee {
-    char[] name;
+    char name[20];
     int hireDate;
     float salary;
-  }
+  };
   
   struct Employee emp1 = {"Jake Peterson", 10, 100000.01};
   
-  printf("%s, %i, %f", emp1.name, emp1.hireDate, emp1.salary);
+  printf("%s, %i, %.2f", emp1.name, emp1.hireDate, emp1.salary);
   
   return 0;
 }
