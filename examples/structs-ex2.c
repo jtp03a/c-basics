7aad65258aa5a6ab791999465fdbb4c0291f5910
Jake Peterson <a3sir86@gmail.com>
2023-02-02 11:11:03 -0600

    Update structs-ex2.c

--- examples/structs-ex2.c
+++ examples/structs-ex2.c
@@ -11,17 +11,23 @@ void readItem(struct * item);
 void printItem(struct * item);
 
 void readItem(struct * item) {
-  char productName[100];
+  char tempStr[100];
   int price;
+  int quantity;
+  
+  // Initial memory allocation 
+  item->itemName = (char*)malloc(15 * sizeof(char));
+
   
   printf("Enter the product name: ");
-  scanf("%s", productName);
+  scanf("%s", tempStr);
+  strcpy(item->itemName, tempStr);
+  
   printf("Enter the produce price: ");
   scanf("%f", &price);
   printf("Enter the product quantity: ");
   scanf("%i", &quantity);
   
-  item->itemName = prodcutName;
   item->price = price;
   item->quantity = quantity;
   
