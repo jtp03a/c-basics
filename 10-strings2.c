8e9ec65f4f49c6cb734a576a7a0e432fedbb76e0
Jake Peterson <a3sir86@gmail.com>
2023-02-03 11:26:40 -0600

    Update 10-strings2.c

--- 10-strings2.c
+++ 10-strings2.c
@@ -66,5 +66,28 @@ int main() {
     tokenized = strtok(NULL, s);
   }
   
+  //analyzing strings
+  char buf[100]; //input buffer
+  int nLetters = 0; //number of letters in input
+  int nDigits = 0; //number of digits in input
+  int nPunct = 0; //number of punctuation characters
+  
+  printf("Enter an interesting string of less than %d characters:\n", 100);
+  scanf("%s", buf); //read a string into the buffer
+  
+  int i = 0; //buffer index
+  while(buf[i]) {
+    if(isalpha(buf[i])) {
+      ++nLetters;
+    } else if (isdigit(buf[i])) {
+      ++nDigits;
+    } else if (ispunct(buf[i])) {
+      ++nPunct;
+    }
+    ++i;
+  }
+  
+  printf("\nYour string contained %d letters, %d digits, and  %d punctuation characters. \n", nLetters, nDigits, nPunct);
+  
   return 0;
 }
