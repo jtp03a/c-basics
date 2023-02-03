4a3cc3fa848ff7dee6b9cffb862e8fe2d69c5d29
Jake Peterson <a3sir86@gmail.com>
2023-02-03 13:03:37 -0600

    Update string-ex1.c

--- examples/string-ex1.c
+++ examples/string-ex1.c
@@ -4,7 +4,8 @@
 #include <stdbool.h>
 
 int countChars(char str[]);
-void concatStr(char str1[], char str2[], char result[]);
+void concatStr(const char str1[], const char str2[], char result[]);
+bool compareStr(const char str1[], const char str2[]);
 
 int countChars(char str[]) {
   int count = 0;
@@ -14,7 +15,7 @@ int countChars(char str[]) {
     return count;
 }
 
-void concatStr(char str1[], char str2[], char result[]) {
+void concatStr(const char str1[], const char str2[], char result[]) {
   int currentPos = 0;
   for (int i = 0; str1[i] != '\0'; i++) {
     result[currentPos] = str1[i];
@@ -27,16 +28,32 @@ void concatStr(char str1[], char str2[], char result[]) {
   }
 }
 
+bool compareStr(const char str1[], const char str2[]) {
+    int str1len = countChars(str1);
+    int str2len = countChars(str2);
+    
+    if (str1len != str2len) {
+        return false;
+    }
+    
+    for (int i = 0; str1[i] != '\0'; i++) {
+        if (str1[i] != str2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main() {
   
   char testString[] = "Test";
-  char testString2[] = "anotherString";
+  char testString2[] = "Test";
   char result[100];
    
   printf("The string %s has % i characters in it.\n", testString, countChars(testString));
   concatStr(testString, testString2, result);
   printf("The result of concatenation between %s and %s is %s.\n", testString, testString2, result);
-
+  
+  printf("Is %s equal to %s: %s", testString, testString2, compareStr(testString, testString2) ? "True" : "False");
   return 0;
 }
