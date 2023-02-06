e572386f8625d0ee61bb1192a14ef1c0a7988923
Jake Peterson <a3sir86@gmail.com>
2023-02-06 16:19:49 -0600

    Update stings-ex2.c

--- examples/stings-ex2.c
+++ examples/stings-ex2.c
@@ -2,14 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-void reverseStr(char[] str);
+void reverseStr(char str[]);
 
-void reverseStr(char[] str) {
-  
+void reverseStr(char str[]) {
+  strlen(str);
 }
 
 int main(){
+  char buf[100];
+  
+  printf("Enter a string no longer than 100 characters");
+  
+  scanf("%s", buf);
+  
+  reverseString(buf);
 
+  printf("%s\n", buf);
+    
   return 0;
 }
 
