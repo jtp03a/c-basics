9f4c2d8926ab490bfacfb9942bac361bb5612b1d
Jake Peterson <a3sir86@gmail.com>
2023-02-09 09:17:23 -0600

    Update fileio-ex1.c

--- examples/fileio-ex1.c
+++ examples/fileio-ex1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main()
+{
+    FILE *fPntr;
+    int c;
+    int lineCounter;
+    
+    fPntr = fopen("test.txt", "r");
+    
+    if(fPntr != NULL) {
+        while((c=fgetc(fPntr)) != EOF) {
+            printf("%c", c);
+            if (c == '\n') {
+                lineCounter++;
+            }
+        }
+        printf("The total number lines detected is: %i", lineCounter);
+    }
+    
+    fclose(fPntr);
+    fPntr = NULL;
+
+    return 0;
+}
