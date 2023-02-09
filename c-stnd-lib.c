85c0c9bb8168a27bb688e5f0983b3215dd8ec96e
Jake Peterson <a3sir86@gmail.com>
2023-02-09 10:55:45 -0600

    Create c-stnd-lib.c

---
+++ c-stnd-lib.c
@@ -0,0 +1,16 @@
+/********************************
+Notes - C Standard Library
+<stddef.h> - Standard Defintions
+  NULL - null pointer constant
+  ptrdiff_t - int produced by subtracting two pointers
+  size_t - int produced by sizeof
+  wchar_t - int required to hold wide characters
+
+<limits.h> - defined limits for character and integer data types
+  example: INT_MAX - 32767
+  
+<stdbool.h> - definition for working with boolean variables
+  bool type def
+  true = 1
+  false = 0
+*********************************/
