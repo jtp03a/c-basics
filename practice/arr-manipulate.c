0dd04eda1ce6d6b6916836833443c8ffcb0e9f68
Jake Peterson <a3sir86@gmail.com>
2023-03-06 12:18:09 -0600

    Update arr-manipulate.c

--- practice/arr-manipulate.c
+++ practice/arr-manipulate.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int arrayManipulate(int *arr, unsigned int size);
+
+int arrayManipulate(int *arr, unsigned int size) {
+   
+  return 0;
+}
+
+int main(){
+  
+  int arr[] = { 1, 5, 12 };
+  
+  int arrLen = sizeof(arr) / sizeof(arr[0]);
+  
+  int result = arrayManipulate(arr, arrLen);
+  
+  printf("%d\n", result);
+  
+  return 0;
+}
 
