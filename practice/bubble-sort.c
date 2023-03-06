82b8f0ebb9386ee5c7b9185a8fb97f373303fcd8
Jake Peterson <a3sir86@gmail.com>
2023-03-06 09:52:26 -0600

    Update bubble-sort.c

--- practice/bubble-sort.c
+++ practice/bubble-sort.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main () {
+int ascending(int a, int b);
+int descending(int a, int b);
+int *bubbleSort(int elements[], size_t length, int (*compare)(int a, int b));
 
+int ascending(int a, int b) {
+  printf("DEBUG: a: %d, b: %d\n", a, b);
+  return a > b;
+}
+
+int descending(int a, int b) {
+  return a < b;
+}
+
+int *bubbleSort(int elements[], size_t length, int (*compare)(int a, int b)) {
+  int i , j;
+  for (i = 0; i < length; i++) {
+    for (j=0; j < length - i - 1; j++) {
+      //check the current element and the next element against the comparator
+      if (compare(elements[j], elements[j+1])) {
+        //if the comparator returns true then swap the current element with the next one
+        int temp = elements[j];
+        elements[j] = elements[j+1];
+        elements[j+1] = temp;
+      }
+    }
+  }
+}
+
+int main () {
+  
+  int result = 0;
+  
+  //Tests
+  result = ascending(10, 5);
+  printf("Ascending, 10, 5, Exp: 1 (true): Result: %d\n", result);
+  
+  result = ascending(5, 10);
+  printf("Ascending, 5, 10, Exp: 0 (false): Result: %d\n", result);
+                     
+  result = descending(5, 10);
+  printf("Ascending, 5, 10, Exp: 1 (true): Result: %d\n", result);                   
+                     
+  result = descending(10, 5);
+  printf("Ascending, 10, 5, Exp: 0 (false): Result: %d\n", result);
+  
+  int arr[] = {5, 1, 4, 2, 8};
+  
+  int (*pAscending)(int, int) = &ascending; //delcare and initialize function pointers
+  int (*pDescending)(int, int) = &descending;
+  
+  size_t length = sizeof(arr) / sizeof(arr[0]); //get length of the array
+  
+  printf("length: %ld\n", length);
+                                    
+  bubbleSort(arr, length, descending);
+                                    
+  for (int i = 0; i < length; i++) {
+    printf("%d, ", arr[i]);
+  }
+  
   return 0;
 }
