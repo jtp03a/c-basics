518c068a82a6cb986ca3fafaaccb747956b00396
Jake Peterson <a3sir86@gmail.com>
2023-03-06 10:19:15 -0600

    Update bubble-sort.c

--- practice/bubble-sort.c
+++ practice/bubble-sort.c
@@ -27,6 +27,7 @@ int *bubbleSort(int elements[], size_t length, int (*compare)(int a, int b)) {
       }
     }
   }
+  return elements;
 }
 
 int main () {
@@ -34,17 +35,17 @@ int main () {
   int result = 0;
   
   //Tests
-//   result = ascending(10, 5);
-//   printf("Ascending, 10, 5, Exp: 1 (true): Result: %d\n", result);
+  result = ascending(10, 5);
+  printf("Ascending, 10, 5, Exp: 1 (true): Result: %d\n", result);
   
-//   result = ascending(5, 10);
-//   printf("Ascending, 5, 10, Exp: 0 (false): Result: %d\n", result);
+  result = ascending(5, 10);
+  printf("Ascending, 5, 10, Exp: 0 (false): Result: %d\n", result);
                      
-//   result = descending(5, 10);
-//   printf("Ascending, 5, 10, Exp: 1 (true): Result: %d\n", result);                   
+  result = descending(5, 10);
+  printf("Ascending, 5, 10, Exp: 1 (true): Result: %d\n", result);                   
                      
-//   result = descending(10, 5);
-//   printf("Ascending, 10, 5, Exp: 0 (false): Result: %d\n", result);
+  result = descending(10, 5);
+  printf("Ascending, 10, 5, Exp: 0 (false): Result: %d\n", result);
   
   int arr[] = {5, 1, 4, 2, 8};
   
@@ -54,12 +55,26 @@ int main () {
   size_t length = sizeof(arr) / sizeof(arr[0]); //get length of the array
   
   printf("length: %ld\n", length);
+
+  int *pointer;
                                     
-  bubbleSort(arr, length, descending);
-                                    
-  for (int i = 0; i < length; i++) {
-    printf("%d, ", arr[i]);
-  }
+  pointer = bubbleSort(arr, length, ascending);
   
+  printf("The pointer is pointing to the first element of the sorted array, Exp: 1: Result: %d\n", *pointer);
+  
+  for (int i = 0; i < length; i++, pointer++) {
+    printf("%d ", *pointer);
+  }
+  printf("\n");
+                     
+  pointer = bubbleSort(arr, length, descending);
+  
+  printf("The pointer is pointing to the first element of the sorted array, Exp: 8: Result: %d\n", *pointer);
+
+  for (int i = 0; i < length; i++, pointer++) {
+    printf("%d ", *pointer);
+  }
+  printf("\n");
+
   return 0;
 }
