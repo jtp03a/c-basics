b28e4d425e837b463a5a21edc39d0a14df0d6145
Jake Peterson <a3sir86@gmail.com>
2023-03-08 14:22:25 -0600

    Update buy_groceries.c

--- practice/buy_groceries.c
+++ practice/buy_groceries.c
@@ -7,6 +7,10 @@ const double itemPrices[] = {3.50, 2.25, 1.99, 4.15};
 
 int buyGroceries(int stuff[], int size)
 {
+    if (size % 2 != 0 || size <= 0 || stuff == NULL) {
+        return 0;
+    }
+    
     struct groceryBasketItem {
         int itemNumber;
         int quantity;
@@ -16,10 +20,13 @@ int buyGroceries(int stuff[], int size)
     struct groceryBasketItem groceryBasket[size / 2];
     
     for (int i = 0; i < size; i+=2) {
-        //item numbers
-        groceryBasket[i/2].itemNumber = stuff[i];
-        groceryBasket[i/2].quantity = stuff[i+1];
-        groceryBasket[i/2].itemCost = itemPrices[stuff[i] - 1];
+        if ((stuff[i] >= 1 && stuff[i] <= 4) && stuff[i+1] > 0){
+            groceryBasket[i/2].itemNumber = stuff[i];
+            groceryBasket[i/2].quantity = stuff[i+1];
+            groceryBasket[i/2].itemCost = itemPrices[stuff[i] - 1];
+        } else {
+            return 0;
+        }
     }
     
    
@@ -27,7 +34,15 @@ int buyGroceries(int stuff[], int size)
     
     for (int i = 0; i < size / 2; i++) {
         printf("Item Number: %d, Quantity: %d, Item Cost: %f\n", groceryBasket[i].itemNumber, groceryBasket[i].quantity, groceryBasket[i].itemCost);
-        total += (groceryBasket[i].quantity * groceryBasket[i].itemCost);
+
+        double itemTotal = groceryBasket[i].quantity * groceryBasket[i].itemCost;
+        
+        if (groceryBasket[i].quantity >= 5) {
+            itemTotal = itemTotal - (itemTotal * 0.05);
+        }
+        
+        total += itemTotal;
+
     }
     
     total = round(total);
@@ -37,13 +52,13 @@ int buyGroceries(int stuff[], int size)
 
 int main() {
     
-    int cart[] = {1, 5, 2, 3, 4, 4};
+    int cart[] = {1, 5, 2, 3, 4, 4, 3, 2};
     
     int size = sizeof(cart) / sizeof(cart[0]);
     
     int result = buyGroceries(cart, size);
     
-    printf("The total cost is: %d: \n", result);
+    printf("The total cost is: %d\n", result);
     
     return 0;
 }
