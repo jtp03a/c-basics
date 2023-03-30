b75900bd290044507dc141843cee17fe5251b00e
Jake Peterson <a3sir86@gmail.com>
2023-03-30 10:40:14 -0500

    Update doubly_ll.c

--- practice/doubly_ll.c
+++ practice/doubly_ll.c
@@ -16,15 +16,22 @@ typedef struct nameNode
 nameNode *buildList(const char **names, int size)
 {
     nameNode *head = (nameNode*) malloc(sizeof(nameNode));
-
-    
+    printf("%d\n", size);
     for (int i = 0; i < size; i++) {
         nameNode *newNode = (nameNode*) malloc(sizeof(nameNode));
-        
         newNode->name = names[i];
         newNode->next = NULL;
         newNode->prev = NULL;
-        head = newNode;
+
+        // first node in list
+        if (head == NULL ) {
+            head = newNode;
+        //insert new node at head
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
     }
     
     return head;
@@ -49,7 +56,25 @@ int main() {
     
     printf("The name in head is : %s\n", head->name);
     
+    //Traverse list
+    nameNode *current = (nameNode*) malloc(sizeof(nameNode));
+    current = head;
+    
+    //traverse to end
+    while (current->next != NULL) {
+        printf("%s\n", current->name);
+        current = current->next;
+    }
+    printf("Data at the tail: %s\n", current->name);
+    // traverse in reverse
+    // while (current != NULL) {
+    //     printf("%s\n", current->name);
+    //     current = current->prev;
+    // }
+    
+    
     
     
     return 0;
 }
+
